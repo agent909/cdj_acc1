e8ebb0f54daf8cdaa0ee17bc6173142bf753cb61
juli <agentvis2@gmail.com>
2018-08-30 15:32:35 +0800

    added apps to do and book app

---
+++ cdj_acc/book/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # url(r'^$', views.index, name='index'),
+    # url(r'^client$', views.register_client, name='register client'),
+    # url(r'^registerUser$', views.register_user, name='register_user')
+]
