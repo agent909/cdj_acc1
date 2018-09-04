cf834f22f4c1da131a472572878b6282f29f1f36
Julius Visto <agentvis2@gmail.com>
2018-09-04 19:46:02 +0800

    added accounts

--- cdj_acc/entry/models.py
+++ cdj_acc/entry/models.py
@@ -14,4 +14,23 @@ class AccountReceivable(models.Model):
     particulars = models.CharField(max_length=500)
     cash = models.DecimalField(decimal_places=5)
 
+
+class CashAdvance(models.Model):
+    entry = models.ForeignKey('Entry', on_delete=models.CASCADE)
+    date = models.DateTimeField('Document Date')
+    cashVoucherNumber = models.PositiveIntegerField()
+    payee = models.CharField(max_length=180)
+    cash = models.DecimalField(decimal_places=5)
+
+
+class AccountPayable(models.Model):
+    entry = models.ForeignKey('Entry', on_delete=models.CASCADE)
+    date = models.DateTimeField('Dacument Date')
+    documentNumber = models.PositiveIntegerField()
+    item = models.CharField(max_length=200)
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(decimal_places=5)
+    totalAmount = models.DecimalField(decimal_places=5)
+
+
 # Create your models here.
