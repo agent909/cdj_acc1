13c7786fe89b29bf4112aa8a4f6a431acb447821
Julius Visto <agentvis2@gmail.com>
2018-09-08 16:04:05 +0800

    added excel files and change model of entry

--- cdj_acc/entry/models.py
+++ cdj_acc/entry/models.py
@@ -2,11 +2,17 @@ from django.db import models
 from register.models import Client
 
 
-class Entry(models.Model):
+class Cash(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
     debit = models.DecimalField(max_digits=12, decimal_places=2, default=0)
     credit = models.DecimalField(max_digits=12, decimal_places=2, default=0)
-    date = models.DateTimeField('Entry Date')
-    account_id = models.PositiveIntegerField(default=0)
+    date = models.DateTimeField('Document Date')
+
+    def __str__(self):
+        if(self.debit == 0):
+            return "credit: "+str(self.credit)
+        else:
+            return "debit: "+str(self.debit)
 
 
 class AccountReceivable(models.Model):
@@ -46,5 +52,67 @@ class AccountPayable(models.Model):
 class LoansPayable(models.Model):
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
     date = models.DateTimeField('Date Issued')
+    lending_company = models.CharField('Lending Company', max_length=200)
+    amount = models.DecimalField(max_digits=12, decimal_places=2)
+    service_fee = models.DecimalField(max_digits=12, decimal_places=2)
+    payable_in = models.PositiveIntegerField('Years to be paid', default=0)
+    interest = models.DecimalField(max_digits=5, decimal_places=2)
+
+# --------------------------------------------------------------------
+
+class AccountReceivableEntry(models.Model):
+    account_receivable = models.ForeignKey(AccountReceivable, on_delete=models.CASCADE)
+    debit = models.DecimalField(max_digits=12, decimal_places=2, default=0)
+    credit = models.DecimalField(max_digits=12, decimal_places=2, default=0)
+    date = models.DateTimeField('Entry Date')
+    # account_id = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        if(self.debit == 0):
+            return "credit: "+str(self.credit)
+        else:
+            return "debit: "+str(self.debit)
+
+
+class CashAdvanceEntry(models.Model):
+    account_receivable = models.ForeignKey(CashAdvance, on_delete=models.CASCADE)
+    debit = models.DecimalField(max_digits=12, decimal_places=2, default=0)
+    credit = models.DecimalField(max_digits=12, decimal_places=2, default=0)
+    date = models.DateTimeField('Entry Date')
+    # account_id = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        if(self.debit == 0):
+            return "credit: "+str(self.credit)
+        else:
+            return "debit: "+str(self.debit)
+
+
+class AccountPayableEntry(models.Model):
+    account_receivable = models.ForeignKey(AccountPayable, on_delete=models.CASCADE)
+    debit = models.DecimalField(max_digits=12, decimal_places=2, default=0)
+    credit = models.DecimalField(max_digits=12, decimal_places=2, default=0)
+    date = models.DateTimeField('Entry Date')
+    # account_id = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        if(self.debit == 0):
+            return "credit: "+str(self.credit)
+        else:
+            return "debit: "+str(self.debit)
+
+
+class LoansPayableEntry(models.Model):
+    account_receivable = models.ForeignKey(LoansPayable, on_delete=models.CASCADE)
+    debit = models.DecimalField(max_digits=12, decimal_places=2, default=0)
+    credit = models.DecimalField(max_digits=12, decimal_places=2, default=0)
+    date = models.DateTimeField('Entry Date')
+    # account_id = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        if(self.debit == 0):
+            return "credit: "+str(self.credit)
+        else:
+            return "debit: "+str(self.debit)
 
 #Create your models here.
